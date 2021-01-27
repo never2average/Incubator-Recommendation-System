57fb03ad8183560aa47479a857a326a3547e2dcb
Mynk-9 <Mynk-9@users.noreply.github.com>
2021-01-28 04:32:37 +0530

    Fixes in custom card and addition of script to create cards from template

---
+++ frontend/assets/js/custom-card-template.js
@@ -0,0 +1,25 @@
+// 6xN array of type:
+// [
+//     [Name, AI Score, Chances of Selection, Funding, Link1, Link2],
+//     [Name, AI Score, Chances of Selection, Funding, Link1, Link2],
+//     [Name, AI Score, Chances of Selection, Funding, Link1, Link2]
+// ]
+const generateCustomCards = (params) => {
+    for (const param of params) {
+        let modalBody = document.querySelector("div.modal-dialog[role=document] div.modal-body div.custom-card-wrapper");
+        let template = document.querySelector('#custom-card-template');
+
+        let clone = template.content.cloneNode(true);
+        let dataFields = clone.querySelectorAll('div.custom-card-data');
+        let title = clone.querySelector('.custom-card-header');
+        let arrow = clone.querySelector('img');
+
+        title.innerHTML = `<a href="${param[4]}">${param[0]}</a>`;
+        dataFields[0].innerHTML = param[1];
+        dataFields[2].innerHTML = param[2];
+        dataFields[4].innerHTML = param[3];
+        arrow.addEventListener('click', () => {location.href = param[5]});
+
+        modalBody.appendChild(clone);
+    }
+}
