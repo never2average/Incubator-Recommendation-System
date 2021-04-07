64fe82e05c49a216ee296ff0e9e84d060cd15a80
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-04-07 18:24:43 +0530

    Temporary deployment coverups

--- frontend/assets/js/custom-card-template.js
+++ frontend/assets/js/custom-card-template.js
@@ -30,15 +30,15 @@ const getRecommendations = () => {
                 //generateCustomCards([['Name1', '55', '25', '35', 'L1.1', 'L1.2'],['Name2', '4', '5', '6', 'L2.1', 'L2.2'],['Name3', '7', '8', '9', 'L3.1', 'L3.2']]);
                 const companies = res.companies[0];
                 let companyList = [];
-                for(const company in companies) {
+                for (const company in companies) {
                     let thisCompany = [];
                     thisCompany.push(company);
                     for (const data of companies[company]) {
                         thisCompany.push(data);
                     }
+                    thisCompany = generateDummyCity(thisCompany);  // temporary fixture
                     companyList.push(thisCompany);
                 }
-
                 generateCustomCards(companyList);
             },
             500: () => {
@@ -48,11 +48,26 @@ const getRecommendations = () => {
     });
 
 };
-const generateCustomCards = (params) => {
-    for (const param of params) {
-        let modalBody = document.querySelector("div.modal-dialog[role=document] div.modal-body div.custom-card-wrapper");
-        let template = document.querySelector('#custom-card-template');
 
+// temporary function, supposed to be removed later
+const generateDummyCity = (data) => {
+    if (data[0] == 'company1') 
+        data.push('Jaipur');
+    else if (data[0] == 'company2')
+        data.push('Mumbai');
+    else if (data[0] == 'company3')
+        data.push('Delhi');
+    data[0] = data[0].replace('company', 'incubator');
+    return data;
+};
+
+// params = ['inc name', 'score1', 'score2', 'score3', 'link1', 'link2', 'city']
+//              0           1          2         3        4        5        6
+const generateCustomCards = (params) => {
+    let modalBody = document.querySelector("div.modal-dialog[role=document] div.modal-body div.custom-card-wrapper");
+    let template = document.querySelector('#custom-card-template');
+
+    for (const param of params) {
         let clone = template.content.cloneNode(true);
         let dataFields = clone.querySelectorAll('div.custom-card-data');
         let title = clone.querySelector('.custom-card-header');
@@ -65,6 +80,7 @@ const generateCustomCards = (params) => {
         arrow.addEventListener('click', () => { location.href = param[5] });
 
         modalBody.appendChild(clone);
+        modalBody.lastElementChild.setAttribute("city", param[6]);
     }
 };
 
@@ -72,6 +88,25 @@ const handleModalClose = () => {
     $('#perfectIncubatorModal').on('hidden.bs.modal', (e) =>
         $("#perfectIncubatorModal div.custom-card").each((index, element) => element.remove())
     );
+};
+
+const cityFilterChange = (selectElement) => {
+    const city = selectElement.value;
+    // console.log(city);
+    let cards = document.querySelector("div.modal-dialog[role=document] div.modal-body div.custom-card-wrapper").children;
+
+    if (city === "Location Filter") {
+        for (const card of cards)
+            card.style.display = "";
+        return;
+    }
+
+    for (const card of cards) {
+        if (card.getAttribute('city') !== city)
+            card.style.display = "none";
+        else
+            card.style.display = "";
+    }
 };
 
 handleModalClose();
