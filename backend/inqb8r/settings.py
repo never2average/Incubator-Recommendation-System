447549e8f312a1d00863163e2a47c909c3af1b94
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-05-03 10:35:57 +0530

    Updated database settings

--- backend/inqb8r/settings.py
+++ backend/inqb8r/settings.py
@@ -82,9 +82,17 @@ WSGI_APPLICATION = 'inqb8r.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'inqb8r',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
