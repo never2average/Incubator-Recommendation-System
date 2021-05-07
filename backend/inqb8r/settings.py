ec4b66bdb2d3730ef797be389f87f7bb02b36dda
Mayank Mathur <mayank.mathur.mayank@outlook.com>
2021-05-07 09:29:28 +0530

    Fixed username of database

--- backend/inqb8r/settings.py
+++ backend/inqb8r/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'inqb8r',
-        'USER': 'inqb8r',
+        'USER': 'postgres',
         'PASSWORD': 'root',
         'HOST': '127.0.0.1',
         'PORT': '5432',
